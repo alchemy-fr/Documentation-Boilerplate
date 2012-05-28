57ef9ad5f91ab986d662b7db44c3cf146200e5c0
grosroro <grosroro@neutron>
2012-05-28 15:33:57 +0200

    Move ribbon to template

--- boilerplate/source/local_conf.py
+++ boilerplate/source/local_conf.py
@@ -2,9 +2,6 @@
 # project name
 project = u'Project Name'
 
-#github repository URL
-github_repo = ''
-
 # Tex info conf
 texinfo_documents = [
   ('index', 'Project name', u'Project Documentation',
